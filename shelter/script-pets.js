0dd3a5e13085d5e3bae9ccfbbdad9dba0e5fa8ff
alisebeth <elizaveta.05031996@yandex.ru>
2023-08-25 16:43:36 +0300

    feat: start pets page slider

--- shelter/script-pets.js
+++ shelter/script-pets.js
@@ -3,15 +3,16 @@ const btn2 = document.querySelector('.btn2');
 const navPage2 = document.querySelector('.header-nav');
 const menuClose = document.querySelector('main');
 const menuPoint = document.querySelectorAll('.li-page');
+const screenWidth = window.screen.availWidth
+
+
+window.addEventListener('resize', function () { 
+    "use strict";
+    window.location.reload(); 
+});
 
 
 btn2.addEventListener("click", menuTwo);
-
-menuClose.addEventListener("click", menuClosed);
-
-for(var i = 0; i < menuPoint.length; i++) {
-    menuPoint[i].addEventListener("click", menuClosed);
-}
 
 function menuTwo(event) {
     
@@ -25,6 +26,13 @@ function menuTwo(event) {
 
 };
 
+
+
+menuClose.addEventListener("click", menuClosed);
+
+for(var i = 0; i < menuPoint.length; i++) {
+    menuPoint[i].addEventListener("click", menuClosed);
+}
 function menuClosed(event)  {
     if (btn2.classList.contains('burger-turn')) {
         btn2.classList.toggle('burger-turn');
@@ -36,3 +44,44 @@ function menuClosed(event)  {
         } 
     } 
 };
+
+
+const petsCards= document.querySelectorAll('.card');
+
+const fistArrow = document.querySelector('.first-ar');
+const lastArrow = document.querySelector('.last-ar');
+const prevArrow = document.querySelector('.prev-ar');
+const nextArrow = document.querySelector('.next-ar');
+let a = 1;
+
+
+
+if (screenWidth <= 767) {
+    nextArrow.classList.remove('inactive-arrow');
+    nextArrow.classList.add('active-arrow');
+    nextArrow.addEventListener("click", nextSlide);
+};
+
+
+function nextSlide(event) {
+    if (a >= petsCards.length - 3) { 
+        petsCards[a - 1].classList.add('disabled-for-320');
+        petsCards[a + 2].classList.remove('disabled-for-320');
+        nextArrow.classList.remove('active-arrow');
+        nextArrow.classList.add('inactive-arrow');
+    } else { 
+        petsCards[a - 1].classList.add('disabled-for-320');
+        petsCards[a + 2].classList.remove('disabled-for-320');
+        a++;
+    } 
+};
+
+if ((screenWidth <= 767) && (a > 1)) {
+    prevArrow.classList.remove('inactive-arrow');
+    prevArrow.classList.add('active-arrow');
+    prevArrow.addEventListener("click", previousSlide);
+};
+
+function previousSlide(event) {
+    console.log('hi mark')
+}
